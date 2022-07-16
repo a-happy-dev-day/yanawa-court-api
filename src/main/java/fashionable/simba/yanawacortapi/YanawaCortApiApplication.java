6d42f4509c90056474b75937284f9b35c813c62e
tis <92219795+this-is-spear@users.noreply.github.com>
2022-07-16 13:39:51 +0900

    Init : Spring 프로젝트 초기세팅

---
+++ src/main/java/fashionable/simba/yanawacortapi/YanawaCortApiApplication.java
@@ -0,0 +1,13 @@
+package fashionable.simba.yanawacortapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YanawaCortApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YanawaCortApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fashionable/simba/yanawacortapi/YanawaCortApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fashionable.simba.yanawacortapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YanawaCortApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

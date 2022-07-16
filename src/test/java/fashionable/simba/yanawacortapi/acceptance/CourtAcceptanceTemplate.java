1d7c27d3d6d2acd93f7173e3c74040fa0eaf09ce
tis <92219795+this-is-spear@users.noreply.github.com>
2022-07-16 15:12:11 +0900

    Test : 인수테스트 작성

---
+++ src/test/java/fashionable/simba/yanawacortapi/acceptance/CourtAcceptanceTemplate.java
@@ -0,0 +1,27 @@
+package fashionable.simba.yanawacortapi.acceptance;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+
+public class CourtAcceptanceTemplate {
+
+    public static ExtractableResponse<Response> 데이터_저장() {
+        return RestAssured.given().log().all()
+            .when().post("/v1/api/courts")
+            .then().log().all().extract();
+    }
+
+    public static ExtractableResponse<Response> 데이터_조회(String name) {
+        return RestAssured.given().log().all()
+            .param("name", name)
+            .when().get("/v1/api/courts")
+            .then().log().all().extract();
+    }
+
+    public static ExtractableResponse<Response> 데이터_목록_조회() {
+        return RestAssured.given().log().all()
+            .when().get("/v1/api/courts")
+            .then().log().all().extract();
+    }
+}

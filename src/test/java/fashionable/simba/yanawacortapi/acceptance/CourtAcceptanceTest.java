9940d0ae4ee07a7f47465b288d07563fe4ec57a8
tis <92219795+this-is-spear@users.noreply.github.com>
2022-07-16 14:09:54 +0900

    Feat : 인수테스트 작성

---
+++ src/test/java/fashionable/simba/yanawacortapi/acceptance/CourtAcceptanceTest.java
@@ -0,0 +1,37 @@
+package fashionable.simba.yanawacortapi.acceptance;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CourtAcceptanceTest extends AcceptanceTest {
+    
+    @Test
+    @DisplayName("공공 데이터 API에서 데이터 목록을 조회해 저장하고 목록을 조회합니다.")
+    void test1() {
+
+    }
+
+    @Test
+    @DisplayName("이름을 입력해 코트장을 검색해 데이터를 조회합니다.")
+    void test2() {
+
+    }
+
+    @Test
+    @DisplayName("지역을 입력해 코트장을 검색해 데이터를 조회합니다.")
+    void test3() {
+
+    }
+
+    @Test
+    @DisplayName("정확한 이름과 지역을 입력해 코트장의 데이터를 한 건만 조회합니다.")
+    void test4() {
+
+    }
+
+    @Test
+    @DisplayName("서울시가 아닌 지역의 이름을 검색하면 데이터가 조회되지 않습니다.")
+    void test5() {
+
+    }
+}

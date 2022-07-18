16bf0c2ecb9bc238a8c35b258002591ded649784
tis <92219795+this-is-spear@users.noreply.github.com>
2022-07-18 13:06:19 +0900

    Feat : 중복된 데이터 삭제

--- src/main/java/fashionable/simba/yanawacortapi/application/CourtFeignApiTranslator.java
+++ src/main/java/fashionable/simba/yanawacortapi/application/CourtFeignApiTranslator.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Component;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Component
@@ -43,7 +44,7 @@ public class CourtFeignApiTranslator {
                 new TypeReference<>() {
                 }
             );
-            return apiResponses.stream().map(apiResponse -> new Court(null, apiResponse.getAreaName(), apiResponse.getPlaceName(), apiResponse.getImagePath())).collect(Collectors.toList());
+            return apiResponses.stream().distinct().map(apiResponse -> new Court(null, apiResponse.getAreaName(), apiResponse.getPlaceName(), apiResponse.getImagePath())).collect(Collectors.toList());
         } catch (Exception e) {
             log.warn("Failed to find list, message is {}", e.getMessage());
             throw new IllegalArgumentException();
